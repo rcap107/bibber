c060418b63e51f2f0be8247f0a6f802eedeecb3f
Riccardo Cappuzzo <riccardo.cappuzzo@gmail.com>
2023-03-09 00:03:30 +0100

    Updated code.

--- main.py
+++ main.py
@@ -4,34 +4,47 @@ import json
 from urllib.parse import quote_plus
 from pypdf import PdfReader
 import re
+from pathlib import Path
 
-#%% 
-### working with doi
-test_doi_url = "https://doi.org/10.1145/3318464.3389742"
+url = "https://doi.org/"
+header = {"Accept": "application/x-bibtex"}
 
-header = {"Accept": "text/x-bibliography"}
+# Regex from https://stackoverflow.com/a/24246270/3741342
+pattern = re.compile(r"(\b10\.(\d+\.*)+[\/](([^\s\.])+\.*)+\b)")
 
-r = requests.get(test_doi_url, headers=header)
-
-if r.status_code == 200:
-    print(r.text)
+# %%
+def parse_pdf_file(pdf_path):
+    reader = PdfReader(pdf_path)
+    page = reader.pages[0]
+    text = page.extract_text()
+    return text
 
 #%%
-reader = PdfReader("embdi-paper.pdf")
-page = reader.pages[0]
-text = page.extract_text()
+def prepare_citations(pdf_dir,pattern):
+    citations = []
 
-p = re.compile(r"(\b10\.(\d+\.*)+[\/](([^\s\.])+\.*)+\b)")
-matches = p.findall(text)
-# %%
-url = "https://doi.org/"
-for match in matches:
-    doi = match[0]
-    header = {"Accept": "application/x-bibtex"}
-    test_doi_url = url + doi
-    r = requests.get(test_doi_url, headers=header)
-    if r.status_code == 200:
-        print(r.text)
+    for f in Path(pdf_dir).iterdir():
+        if f.is_file() and f.suffix == ".pdf":
+            text = parse_pdf_file(f)
+            matches = pattern.findall(text)
+            for match in matches:
+                doi = match[0]
+                test_doi_url = url + doi
+                r = requests.get(test_doi_url, headers=header)
+                if r.status_code == 200:
+                    cit = r.text
+                    cit = cit.replace("%2F", "/")
+                    citations.append(cit)
+                    break
+    return citations
 
 
+#%%
+if __name__ == "__main__":
+    pdf_dir = "."
+    citations = prepare_citations(pdf_dir, pattern)
+    with open("citations.txt", "w") as f:
+        for c in citations:
+            f.write(c + "\n")
+    
 # %%
